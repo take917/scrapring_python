daeb46cf71a46c7a097d98d3a274523b7846f89a
take917 <1tktk2tk3@gmail.com>
2022-06-02 08:43:20 +0900

    🎨 scraping code modification

--- scraping.py
+++ scraping.py
@@ -1,4 +1,5 @@
 
+from turtle import title
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
@@ -10,3 +11,29 @@ driver = webdriver.Chrome(options=options)
 
 url = "https://search.rakuten.co.jp/search/mall/ANKER/?f=0"
 driver.get(url)
+
+time.sleep(2)
+
+item_name = []
+url_list = []
+stock_flg =[]
+
+items=driver.find_elements_by_class_name("searchresultitem")
+for item in items:
+    title = item.find_element_by_class_name("title")
+    item_name.append(title.text)
+
+    url = item.find_element_by_tag_name('a').get_attribute("href")
+    url_list.append(url)
+
+    item = item.text
+    item = item.split('\n')
+
+    if "売り切れ" in item:
+        stock_flg.append(1)
+    else:
+        stock_flg.append(0)
+
+df = pd.DataFrame([item_name,url_list,stock_flg]).T
+print(df)
+driver.quit()
